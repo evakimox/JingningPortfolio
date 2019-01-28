26bfad074e0cea62e4687a4aa370b07496404903
Jingning <zhangjn1121@gmail.com>
2019-01-28 16:53:38 -0500

    some post blocks

--- summaryOrganizer.js
+++ summaryOrganizer.js
@@ -1,8 +1,8 @@
 let posts = [
-    {id:1, title:"hey", imgsrc:"", additional:"hwyhwy"},
-    {id:2, title:"hey2", imgsrc:"", additional:"hwyhwy"},
-    {id:3, title:"hey3", imgsrc:"", additional:"hwyhwy"},
-    {id:4, title:"hey4", imgsrc:"", additional:"hwyhwy"},
+    {id:1, title:"hey", imgsrc:"./pics/bg.jpg", additional:"hwyhwy"},
+    {id:2, title:"hey2", imgsrc:"./pics/824.jpg", additional:"hwyhwy"},
+    {id:3, title:"hey3", imgsrc:"./pics/piano.jpg", additional:"hwyhwy"},
+    {id:4, title:"hey4", imgsrc:"./pics/tsuru.png", additional:"hwyhwy"},
 ];
 
 function mapPosts(){
@@ -10,10 +10,16 @@ function mapPosts(){
     for(let post of posts){
         let singlePost =
             '<div class="column">\n' +
-            '    <div class="content-block">'+
-                    post.title+' '+post.additional+
+            '    <div class="content-block">\n' +
+            '        <div class="post_title">\n' +
+            '            Title\n' +
+            '        </div>\n' +
+            '        <img class="picture_block" src="'+post.imgsrc+'" />\n' +
+            '        <div class="post_desc">\n' +
+            post.additional +
+            '        </div>\n' +
             '    </div>\n' +
-            '</div>';
+            '</div>'
         postContent += singlePost;
     }
     $('#posts').html(postContent);
