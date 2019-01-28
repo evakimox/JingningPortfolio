d40ff3f32242f1bd215d249fc0e655db145b1a7d
Jingning <zhangjn1121@gmail.com>
2019-01-28 18:18:40 -0500

    add icon

--- summaryOrganizer.js
+++ summaryOrganizer.js
@@ -1,21 +1,39 @@
 let posts = [
-    {id:1, title:"Portfolio Cover picture", imgsrc:"./pics/bg.jpg",
-        additional:"As for my portfolio cover page, I want the picture to show to some extent how a person I am. So I put my favorite things to a pattern."
-    },
-    {id:2, title:"Clock with faries", imgsrc:"./pics/824.jpg",
+    {id:6, title:"Clock with faries", imgsrc:"./pics/824.jpg",
         additional:"This picture was inspired by a rainy day, when the temperature drops, and I thought autumn is approaching by the time."
     },
-    {id:3, title:"Kuro Mujo and Shiro Mujo on Piano Keys", imgsrc:"./pics/piano.jpg",
+    {id:5, title:"Kuro Mujo and Shiro Mujo on Piano Keys", imgsrc:"./pics/piano.jpg",
         additional:"Fanart of game Onmyoji"
     },
-    {id:4, title:"Tsurumaru emoji", imgsrc:"./pics/tsuru.png",
-        additional:"Fanart of game Token Ranbu"
-    },
-    {id:5, title:"Nobody Alive", imgsrc:"./pics/login.gif",
+    {id:4, title:"Nobody Alive", imgsrc:"./pics/login.gif",
         additional:"PC indie game"
     },
-    {id:6, title:"Bee Flightened", imgsrc:"./pics/beeFlightened.png",
+    {id:4, title:"Leona", imgsrc:"./pics/leo.jpg",
+        additional:"Costume making of character Leona in game League of Legend"
+    },
+    {id:4, title:"Orianna", imgsrc:"./pics/ori.jpg",
+        additional:"Costume making of character Orianna in game League of Legend"
+    },
+    {id:4, title:"Levi doll", imgsrc:"./pics/Levi.jpg",
+        additional:"Doll making of character Levi in anime Attack on Titans"
+    },
+    {id:4, title:"Kandappa gown", imgsrc:"./pics/kdp.jpg",
+        additional:"Cosplay making of character Kandappa in manga RG-Veda"
+    },
+    {id:4, title:"Soma suit", imgsrc:"./pics/som.jpg",
+        additional:"Cosplay making of character Soma in manga RG-Veda"
+    },
+    {id:3, title:"Bee Flightened", imgsrc:"./pics/beeFlightened.png",
         additional:"Android shooting game"
+    },
+    {id:2, title:"Tsurumaru emoji", imgsrc:"./pics/tsuru.png",
+        additional:"Fanart of game Token Ranbu"
+    },
+    {id:1, title:"Digi Calligraphy", imgsrc:"./pics/helloWorld.png",
+        additional:"Random little inspirations about how a programmer will do arts."
+    },
+    {id:1, title:"Portfolio Cover picture", imgsrc:"./pics/bg.jpg",
+        additional:"As for my portfolio cover page, I want the picture to show to some extent how a person I am. So I put my favorite things to a pattern."
     },
 ];
 
